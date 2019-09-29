cee5d7ad8da202e7bb59c37a8370db6d85ab953b
Mikko <mikko@pursuittechnology.com.au>
2019-09-29 16:55:37 +1000

    Example commit

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,19 +1,12 @@
 // @flow
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes';
 import styles from './Home.css';
 
-type Props = {};
-
-export default class Home extends Component<Props> {
-  props: Props;
-
+export default class Home extends Component {
   render() {
     return (
       <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
+        <h2>ICTP Government Analytical Laboratory</h2>
       </div>
     );
   }
