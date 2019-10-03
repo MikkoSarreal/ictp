294406ffb7bbff7cb36492455f57727fecb76461
Ruichen <chenrui1992@hotmail.com>
2019-10-03 22:25:09 +1000

    Update Name

--- app/components/Home.js
+++ app/components/Home.js
@@ -6,7 +6,7 @@ export default class Home extends Component {
   render() {
     return (
       <div className={styles.container} data-tid="container">
-        <h2>ICTP Government Analytical Laboratory</h2>
+        <h2>ICTP ACTGAL</h2>
       </div>
     );
   }
