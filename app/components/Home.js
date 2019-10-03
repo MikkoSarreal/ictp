c40422db13f13418f2bf46e64339c96eee185b04
Zhendong <zhendongguo428@gmail.com>
2019-10-03 20:58:16 +0800

    Sample commit

--- app/components/Home.js
+++ app/components/Home.js
@@ -6,7 +6,7 @@ export default class Home extends Component {
   render() {
     return (
       <div className={styles.container} data-tid="container">
-        <h2>ICTP ACTGAL</h2>
+        <h2>ICTP Group Project</h2>
       </div>
     );
   }
